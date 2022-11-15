04ab5b4df135bf74881744aa0d3562f71dfa9517
kszczerbowski <kszczerbowski223@gmail.com>
2022-11-15 23:15:03 +0100

    update 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Notify.failure("Please choose a date in the future");
+const inputs = document.querySelectorAll('input');
+const createBtn = document.querySelector('button');
+let firstDelay;
+let delay;
+let promiseAmount;
+const chosenValues = [firstDelay, delay, promiseAmount];
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +15,20 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+inputs.forEach((input, i) => {
+  input.addEventListener('input', () => {
+    chosenValues[i] = input.value;
+  })
+})
+
+createBtn.addEventListener('click', (event) => {
+  if (chosenValues.some((value) =>  value === 1 )) Notify.failure("Please fill in all 3 fields!");
+  console.log(chosenValues.some(value => { value === 'undefined' }))
+  console.log(delay === undefined)
+  event.preventDefault();
+  console.log(chosenValues)
+  console.log(chosenValues.some((element, index, array) => {
+    typeof element === undefined;
+  }));
+})
